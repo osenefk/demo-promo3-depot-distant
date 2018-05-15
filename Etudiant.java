ac8e6e0cbd436b5e4cec166e6d92643637729bba
Ousseynou SENE <seneousseynou89@gmail.com>
2018-05-15 22:08:07 +0100

    Ajout de fichiers source java

---
+++ Etudiant.java
@@ -0,0 +1,14 @@
+public class Etudiant{
+	
+	private String nom;
+	
+	private String prenom;
+	
+	private String email;
+	
+	public Etudiant(){
+		
+	}
+	
+	
+}
---
+++ Formateur.java
@@ -0,0 +1,16 @@
+public class Formateur{
+	
+	private String nom;
+	
+	private String prenom;
+	
+	private String email;
+	
+	private int anneExperience;
+	
+	public Formateur(){
+		
+	}
+	
+	
+}
